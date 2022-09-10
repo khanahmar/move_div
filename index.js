cc46e25fcf193408f62635a1f601e31d3fdc7315
Ahmar khan <ahmarbilal223344@gmail.com>
2022-09-10 10:33:57 -0700

    changes made

--- index.js
+++ index.js
@@ -1,34 +1,66 @@
+/*
+
+1 controle speed of box with slider
+2 return the box when it goes out of the range
+
+*/
+const slider = document.getElementById("slider");
 const box = document.querySelector("#box");
+let srcResolution = `${1366 * 768}PPI `;
 let leftSide = 0;
 let topSide = 0;
+let move = Number(prompt("Enter a number from 1 to 20"));
+
 // function to move box
-
-window.addEventListener("keyup", (e) => {
-  console.log(e.key);
-
-  if (e.key === "ArrowRight") {
-    leftSide = leftSide + 20;
-    box.style.left = `${leftSide}px`;
-  } else if (e.key === "ArrowLeft") {
-    leftSide = leftSide - 20;
-    box.style.left = leftSide + "px";
-  } else if (e.key === "ArrowUp") {
-    topSide = topSide - 20;
-    box.style.top = topSide + "px";
-  } else if (e.key === "ArrowDown") {
-    topSide = topSide + 20;
-    box.style.top = topSide + "px";
-  } else {
-    console.log("invalid key press");
+if (move <= 20 && move >= 0) {
+  if (box.value > leftSide) {
+    alert("cant go more");
   }
+  window.addEventListener("keydown", (e) => {
+    console.log(e.key);
 
-  // switch () {
-  //   case "ArrowRight":
-  //     leftSide = leftSide + 20;
-  //     box.style.left = leftSide + "px";
-  //     break;
+    // if (e.key === "ArrowRight") {
+    //   leftSide = leftSide + 20;
+    //   box.style.left = `${leftSide}px`;
+    // } else if (e.key === "ArrowLeft") {
+    //   leftSide = leftSide - 20;
+    //   box.style.left = leftSide + "px";
+    // } else if (e.key === "ArrowUp") {
+    //   topSide = topSide - 20;
+    //   box.style.top = topSide + "px";
+    // } else if (e.key === "ArrowDown") {
+    //   topSide = topSide + 20;
+    //   box.style.top = topSide + "px";
+    // } else {
+    //   console.log("invalid key press");
+    // }
 
-  //   default:
-  //     break;
-  // }
-});
+    switch (e.key) {
+      case "ArrowRight":
+        leftSide += move;
+        box.style.left = `${leftSide}px`;
+        break;
+
+      case "ArrowLeft":
+        leftSide -= move;
+        box.style.left = leftSide + "px";
+        break;
+
+      case "ArrowDown":
+        topSide += move;
+        box.style.top = topSide + "px";
+        break;
+
+      case "ArrowUp":
+        topSide -= move;
+        box.style.top = topSide + "px";
+        break;
+
+      default:
+        break;
+    }
+    box.innerHTML = `left = ${leftSide} <br> top = ${topSide}`;
+  });
+} else {
+  alert("You have entered a wrong number");
+}
